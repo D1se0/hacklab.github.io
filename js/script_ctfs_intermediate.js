4c74ead2619295e6f2208793f5f5a93557cc00ea
D1se0 <164921056+D1se0@users.noreply.github.com>
2024-04-24 16:54:23 +0200

    Add files via upload

---
+++ js/script_ctfs_intermediate.js
@@ -0,0 +1,20 @@
+function typeTerminal2() {
+    const terminalText2 = "get CTF-intermediate";
+    let index = 0;
+    const terminalElement = document.getElementById("terminal2");
+    terminalElement.innerHTML = ''; // Limpiamos el contenido previo antes de escribir de nuevo
+
+    function typeCharacter() {
+        if (index < terminalText2.length) {
+            terminalElement.innerHTML += terminalText2.charAt(index);
+            index++;
+            setTimeout(typeCharacter, 50); // Ajusta la velocidad de escritura aquí (en milisegundos)
+        } else {
+            setTimeout(typeTerminal2, 10000); // Llama a typeTerminal nuevamente después de 10 segundos
+        }
+    }
+
+    typeCharacter(); // Inicia el proceso de escritura de caracteres
+}
+
+typeTerminal2(); // Inicia el primer ciclo de escritura
