19987a2c6a145643e0c36c5807d8b342377a7d79
D1se0 <164921056+D1se0@users.noreply.github.com>
2024-06-18 21:46:47 +0200

    Add files via upload

---
+++ js/write_up_23.js
@@ -0,0 +1,28 @@
+
+// Función para simular la escritura automática en la terminal
+function typeTerminal() {
+    const terminalText = "cat Basic_Pentesting_1.txt";
+    let index = 0;
+    const terminalElement = document.getElementById("terminalText");
+
+    function typeCharacter() {
+        if (index < terminalText.length) {
+            terminalElement.textContent += terminalText.charAt(index);
+            index++;
+        } else {
+            clearInterval(intervalId);
+            setTimeout(clearTerminal, 10000); // Esperar 1 segundo antes de borrar y comenzar de nuevo
+        }
+    }
+
+    function clearTerminal() {
+        terminalElement.textContent = "root@WriteUpBasic_Pentesting_1~# "; // Restaurar el prefijo
+        index = 0;
+        intervalId = setInterval(typeCharacter, 100); // Iniciar el bucle de nuevo
+    }
+
+    let intervalId = setInterval(typeCharacter, 100);
+}
+
+// Llamar a la función para iniciar la escritura automática
+typeTerminal();
